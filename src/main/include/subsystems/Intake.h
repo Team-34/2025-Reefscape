26b8bd7d203631c72430a143117a7efff16f04e4
Kale Olmsted <kolmsted2009@gmail.com>
2025-02-01 09:24:20 -0600

    Addressed pull request comment

---
+++ src/main/include/subsystems/Intake.h
@@ -0,0 +1,24 @@
+//uses sparkmax for now
+#pragma once
+#include <rev/SparkMax.h>
+#include <frc2/command/SubsystemBase.h>
+#include <frc2/command/InstantCommand.h>
+#include <rev/config/SparkMaxConfig.h>
+using namespace rev::spark;
+
+namespace t34
+{
+    class Intake : public frc2::SubsystemBase {
+        public:
+        
+        Intake();
+
+        frc2::CommandPtr RunIn(double power_percentage);
+        frc2::CommandPtr RunOut(double power_percentage);
+        
+        private:
+
+        SparkMax m_motor_1;
+        SparkMax m_motor_2;
+    };
+}
