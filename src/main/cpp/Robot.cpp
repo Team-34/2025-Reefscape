6213b74cb376ac583c8766ac8ad744647fb60749
Jameothie <james.pearson0109@gmail.com>
2025-02-01 11:01:49 -0600

    updated name of default command

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -25,8 +25,8 @@ void Robot::DisabledExit() {}
 void Robot::AutonomousInit() {
   m_autonomous_command = rc.GetAutonomousCommand();
 
-  if (m_autonomous_Command) {
-    m_autonomous_Command->Schedule();
+  if (m_autonomous_command) {
+    m_autonomous_command->Schedule();
   }
 }
 
@@ -35,10 +35,10 @@ void Robot::AutonomousPeriodic() {}
 void Robot::AutonomousExit() {}
 
 void Robot::TeleopInit() {
-  if (m_autonomous_Command)
+  if (m_autonomous_command)
   {
-    m_autonomous_Command->Cancel();
-    m_autonomous_Command.reset();
+    m_autonomous_command->Cancel();
+    m_autonomous_command.reset();
   }
 
   frc2::CommandScheduler::GetInstance().SetDefaultCommand(rc.swerve_drive.get(), std::move(rc.m_default_command));
