8307ff714cdb13fd63296b078577570478863011
Brad Collins <bsc@housecollins.com>
2025-02-08 13:49:28 -0600

    Formatting only; no functional change

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -20,14 +20,15 @@ Elevator::Elevator()
 
 frc2::CommandPtr Elevator::MoveWristToCommand(units::degree_t angle)
 {
-  return this->RunEnd([this, angle] {
-    m_wrist_motor.Set(m_wrist_motor_pid.Calculate(
-      NEOUnitToDegree(m_wrist_motor.GetEncoder().GetPosition()), NEOUnitToDegree(angle.value())));
-  },
-  [this]{    m_wrist_motor.StopMotor();
-  }).Until([this]{
-    return m_wrist_motor_pid.AtSetpoint();
-  });
+  return this->RunEnd(
+    [this, angle] {
+      m_wrist_motor.Set(m_wrist_motor_pid.Calculate(
+        NEOUnitToDegree(m_wrist_motor.GetEncoder().GetPosition()), NEOUnitToDegree(angle.value())));
+    },
+    [this] {
+      m_wrist_motor.StopMotor();
+    })
+    .Until([this] { return m_wrist_motor_pid.AtSetpoint(); });
 }
 
 frc2::CommandPtr Elevator::ElevateToCommand(units::inch_t height)
