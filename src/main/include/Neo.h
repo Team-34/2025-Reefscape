12f50acdbaba08dfbdeb179eeb8f17b10bf843c1
Brad Collins <bsc@housecollins.com>
2025-02-22 11:08:39 -0600

    Additional test and more units for type safety

--- src/main/include/Neo.h
+++ src/main/include/Neo.h
@@ -8,17 +8,17 @@ namespace t34
 {
   namespace Neo
   {
-    constexpr auto NEO550_RESOLUTION         { 42.0 };
-    constexpr auto NEO550_SHAFT_CIRCUMFERENCE{ units::inch_t{ 1.0 / 8.0 } * M_PI };
+    constexpr auto NEO550_RESOLUTION         { 42.0 / 1_tr };
+    constexpr auto NEO550_SHAFT_CIRCUMFERENCE{ (( 1.0_in / 8.0 ) * M_PI) / 1_tr };
 
     inline constexpr double DegreeTo550Unit(units::degree_t degrees)
     {
-        return (degrees / 360_deg) * NEO550_RESOLUTION;
+      return degrees * NEO550_RESOLUTION;
     }
 
     inline constexpr double InchTo550Unit(units::inch_t inches)
-    {
-        return (inches / NEO550_SHAFT_CIRCUMFERENCE) * NEO550_RESOLUTION;
+    {   
+      return DegreeTo550Unit(inches / NEO550_SHAFT_CIRCUMFERENCE);
     }
 
   } // namespace Neo
