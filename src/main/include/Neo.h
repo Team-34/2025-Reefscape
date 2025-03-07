66e36a77d5b4938fa53b62e403a84614aad75b44
Brad Collins <bsc@housecollins.com>
2025-03-06 19:27:31 -0600

    chore: Formatting; no functional change

--- src/main/include/Neo.h
+++ src/main/include/Neo.h
@@ -12,31 +12,28 @@ namespace t34
     constexpr auto NEO550_SHAFT_CIRCUMFERENCE{ (( 1.0_in / 8.0 ) * M_PI) / 1_tr };
 
     /**
-   * Converts inches into NEO550 encoder units.
-   * 
-   * @returns inches * (1/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES
-   */
-  inline double InchToNEOUnit(units::inch_t inches)
-  {
-      // 42 encoder units = ~0.3927 inches
-      // 1 inch = 1/NEO550_SHAFT_CIRCUMFERENCE_INCH = ~2.546 NEO550 revolutions
-      // 1 inch = (1/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES = ~106.952 encoder units
+     * Converts inches into NEO550 encoder units.
+     * 
+     * @returns inches * (1/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES
+     */
+    inline double InchToNEOUnit(units::inch_t inches)
+    {
+        // 42 encoder units = ~0.3927 inches
+        // 1 inch = 1/NEO550_SHAFT_CIRCUMFERENCE_INCH = ~2.546 NEO550 revolutions
+        // 1 inch = (1/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES = ~106.952 encoder units
 
-      return (1.0/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES * inches.value();
-  }
+        return (1.0/NEO550_SHAFT_CIRCUMFERENCE_INCH) * NEO550_RES * inches.value();
+    }
 
-  /**
-   * Converts degrees into NEO550 encoder units.
-   * 
-   * @returns (NEO550_RES / 360) * degrees
-   */
-  inline double DegreeToNEOUnit(units::degree_t degrees)
-  {
-      return (NEO550_RES / 360.0) * degrees.value();
-  }
-
-
-
+    /**
+     * Converts degrees into NEO550 encoder units.
+     * 
+     * @returns (NEO550_RES / 360) * degrees
+     */
+    inline double DegreeToNEOUnit(units::degree_t degrees)
+    {
+        return (NEO550_RES / 360.0) * degrees.value();
+    }
 
     // inline constexpr double DegreeTo550Unit(units::degree_t degrees)
     // {
