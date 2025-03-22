d95a604d77ea06e187ee818dc433e8d398b7fb64
Brad Collins <bsc@housecollins.com>
2025-03-22 08:53:58 -0500

    Removed a couple of no-longer-necessary-items from Robot.cpp

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -2,9 +2,6 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
-#include <frc2/command/CommandScheduler.h>
-
 #include "Robot.h"
 #include "subsystems/LimelightUtil.h"
 
@@ -44,8 +41,6 @@ void Robot::TeleopInit()
     m_autonomous_command->Cancel();
     m_autonomous_command.reset();
   }
-
-  frc2::CommandScheduler::GetInstance().SetDefaultCommand(rc.swerve_drive.get(), std::move(rc.m_default_command));
 }
 
 void Robot::TeleopPeriodic() {}
